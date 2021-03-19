42676dbe1d428716661c5d738a72a0c7dd30aad6
Viacheslav Lamash <Viacheslav_Lamash@epam.com>
2021-03-19 16:29:46 +0300

    Task babel fix

--- app/task2.js
+++ app/task2.js
@@ -1,13 +1,20 @@
-const csv = require('csvtojson');
-const fs = require('fs');
+import csv from 'csvtojson'
+import fs from 'fs'
 
-const csvFilePath = './app/assets/addresses.csv';
+const csvFilePath = './assets/books.csv';
 const readStream = fs.createReadStream(csvFilePath);
-const writeStream = fs.createWriteStream(`${__dirname}/result.txt`);
+const writeStream = fs.createWriteStream(`result.txt`);
 
 readStream.on('error', function (err) {
     if (err.code == 'ENOENT') {
-        console.log("File not Found!");
+        console.log("File not Found!", err);
+    } else {
+        console.error(err);
+    }
+});
+writeStream.on('error', function (err) {
+    if (err.code == 'ENOENT') {
+        console.log("File not Found!", err);
     } else {
         console.error(err);
     }
